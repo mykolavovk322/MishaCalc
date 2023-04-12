98d49f73e3ddfd9ac861b52ff7ea9921a37904d5
Mykola_Vovk <vovkm322@gmail.com>
2023-04-12 18:15:21 +0300

    add messages to Calc class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         MishaCalc calc = new MishaCalc();
         System.out.println(calc.add(1,1));
+        System.out.println(calc.divide(1,0));
     }
 }
--- src/main/java/org/example/calculator/impl/MishaCalc.java
+++ src/main/java/org/example/calculator/impl/MishaCalc.java
@@ -4,18 +4,22 @@ import org.example.calculator.Calc;
 
 public class MishaCalc implements Calc {
     public int add(int parameter1, int parameter2) {
+        System.out.println("Using add method");
         return parameter1 + parameter2;
     }
 
     public int subtract(int parameter1, int parameter2) {
+        System.out.println("Using subtract method");
         return parameter1 - parameter2;
     }
 
     public int multiply(int parameter1, int parameter2) {
+        System.out.println("Using multiply method");
         return parameter1 * parameter2;
     }
 
     public int divide(int numerator, int denominator) {
+        System.out.println("Using divide method");
         int fraction = 0;
         try {
             fraction = numerator / denominator;
